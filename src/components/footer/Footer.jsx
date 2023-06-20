5474151de4e7350d5f4b23168d668b84a746bf6a
Goat95 <hjh9703@naver.com>
2023-06-20 17:30:37 +0900

    style: add footer style

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,41 @@
+import Image from "next/image";
+
 import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>©2023 JunHyeok. All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Social Image"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Social Image"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Social Image"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Social Image"
+        />
+      </div>
     </div>
   );
 };
