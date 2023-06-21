63dc7e7078ef817c89d92ab242749eb64bebc895
Goat95 <hjh9703@naver.com>
2023-06-22 00:06:13 +0900

    feat: implement blog page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,66 @@
+import Image from "next/image";
+
 import styles from "./page.module.css";
 
 const BlogPost = () => {
-  return <div>BlogPost</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+            voluptatum possimus reiciendis praesentium soluta repellendus
+            distinctio officia rerum odit, vel doloribus dolorum facere at
+            beatae similique provident magnam, numquam corporis.
+          </p>
+          <div className={styles.author}>
+            <Image
+              src=""
+              alt="Avatar"
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>John Doe</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/16353919/pexels-photo-16353919/free-photo-of-fontanna-di-trevi-in-rome-italy.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+            alt="Post Image"
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam
+          itaque sunt nesciunt, aperiam dolor odit numquam distinctio cupiditate
+          possimus, expedita ullam laudantium. Corrupti qui unde, alias
+          architecto optio delectus fugiat.
+          <br />
+          <br />
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam
+          itaque sunt nesciunt, aperiam dolor odit numquam distinctio cupiditate
+          possimus, expedita ullam laudantium. Corrupti qui unde, alias
+          architecto optio delectus fugiat.
+          <br />
+          <br />
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam
+          itaque sunt nesciunt, aperiam dolor odit numquam distinctio cupiditate
+          possimus, expedita ullam laudantium. Corrupti qui unde, alias
+          architecto optio delectus fugiat. Lorem, ipsum dolor sit amet
+          consectetur adipisicing elit. Magnam itaque sunt nesciunt, aperiam
+          dolor odit numquam distinctio cupiditate possimus, expedita ullam
+          laudantium. Corrupti qui unde, alias architecto optio delectus fugiat.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPost;
