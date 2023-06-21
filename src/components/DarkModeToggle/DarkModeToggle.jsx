f37208963b2e9001b57271ad2307f0fc11f94568
Goat95 <hjh9703@naver.com>
2023-06-22 00:23:38 +0900

    feat: implement theme context api using useContext

--- src/components/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -1,9 +1,16 @@
+"use client";
+
+import { useContext } from "react";
+
 import styles from "./darkModeToggle.module.css";
 
+import { ThemeContext } from "../../../context/ThemeContext";
+
 const DarkModeToggle = () => {
-  const mode = "dark";
+  const { toggle, mode } = useContext(ThemeContext);
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={toggle}>
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🔆</div>
       <div
