5498a59365f04765d272a10f9cb39333c7a46470
Goat95 <hjh9703@naver.com>
2023-06-22 19:37:51 +0900

    feat: implement getData function

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,40 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import useSWR from "swr";
+
 import styles from "./page.module.css";
 
 const Dashboard = () => {
+  // const [data, setData] = useState([]);
+  // const [err, setErr] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     setIsLoading(true);
+  //     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`, {
+  //       cache: "no-store",
+  //     });
+
+  //     if (!res.ok) {
+  //       setErr(true);
+  //     }
+
+  //     const data = await res.json();
+
+  //     setData(data);
+  //     setIsLoading(false);
+  //   };
+  //   getData();
+  // }, []);
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
   return <div>Dashboard</div>;
 };
 
